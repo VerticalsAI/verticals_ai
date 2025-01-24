a2c1a090a9076811ba6b7c712d9378e98d977d88
vodinhvien <vodinhvien.work@gmail.com>
2025-01-24 11:36:08 +0700

    feat: auto redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
+"use client"
+import React, { useEffect } from "react";
 
+import { BorderBeam } from "@/components/ui";
 
-import Chat from './(app)/chat/_components/chat';
-import NotLoggedInAlert from './(app)/chat/_components/not-logged-in-alert';
-
+import GraphComponent from "./_components";
+import LoginButton from "./_components/login-button";
+import { useRouter } from "next/navigation";
 
 const Graph = () => {
-    return (
-        <div className="flex-1 h-0 overflow-y-hidden w-full">
-        <Chat />
-        <NotLoggedInAlert />
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/chat");
+  }, [router]);
+
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center h-screen pt-16 pb-4">
+        <div className="w-full h-full flex flex-col items-center justify-center gap-4">
+          <div className="flex flex-col items-center justify-center gap-2 text-center">
+            <h1 className="text-4xl font-bold text-brand-600">
+              Meet Synnax AI
+            </h1>
+            <p className="">A modular network of interoperable DeFi agents</p>
+            <LoginButton />
+          </div>
+          {/* <div className="w-full flex-1 max-w-2xl rounded-md border border-neutral-200 dark:border-neutral-700 relative">
+            <GraphComponent />
+            <BorderBeam />
+          </div> */}
+        </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Graph;

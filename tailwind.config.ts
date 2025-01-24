320cf93d09fabfd7cac21661e2361ee848f252f9
vodinhvien <vodinhvien.work@gmail.com>
2025-01-24 16:45:57 +0700

    fix: font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,83 +1,84 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-		fontFamily: {
-      "gilroy": ["Gilroy", "sans-serif"],
+    fontFamily: {
+      DEFAULT: ["DM Mono", "sans-serif"],
+      gilroy: ["Gilroy", "sans-serif"],
     },
-  	extend: {
-			borderWidth: {
-        DEFAULT: '1px',
+    extend: {
+      borderWidth: {
+        DEFAULT: "1px",
       },
-			borderColor: {
-        DEFAULT: 'rgba(255, 255, 255, 0.05)',
+      borderColor: {
+        DEFAULT: "rgba(255, 255, 255, 0.05)",
       },
-			backgroundColor: {
-				'white-3': 'rgba(255, 255, 255, 0.03)',
-			},
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)',
-  			brand: {
-  				'50': '#ffffe7',
-  				'100': '#ffffc1',
-  				'200': '#fffb86',
-  				'300': '#fff041',
-  				'400': '#ffe00d',
-  				'500': '#ffd100',
-  				'600': '#d19900',
-  				'700': '#a66d02',
-  				'800': '#89550a',
-  				'900': '#74450f',
-  				'950': '#442404'
-  			},
-  			text: {
-  				light: '#000000',
-  				dark: '#ffffff'
-  			},
-  			sidebar: {
-  				// DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))',
-  				active: 'hsl(var(--sidebar-active))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		animation: {
-  			'shiny-text': 'shiny-text 8s infinite',
-  			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear'
-  		},
-  		keyframes: {
-  			'shiny-text': {
-  				'0%, 90%, 100%': {
-  					'background-position': 'calc(-100% - var(--shiny-width)) 0'
-  				},
-  				'30%, 60%': {
-  					'background-position': 'calc(100% + var(--shiny-width)) 0'
-  				}
-  			},
-  			'border-beam': {
-  				'100%': {
-  					'offset-distance': '100%'
-  				}
-  			}
-  		}
-  	}
+      backgroundColor: {
+        "white-3": "rgba(255, 255, 255, 0.03)",
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        brand: {
+          "50": "#ffffe7",
+          "100": "#ffffc1",
+          "200": "#fffb86",
+          "300": "#fff041",
+          "400": "#ffe00d",
+          "500": "#ffd100",
+          "600": "#d19900",
+          "700": "#a66d02",
+          "800": "#89550a",
+          "900": "#74450f",
+          "950": "#442404",
+        },
+        text: {
+          light: "#000000",
+          dark: "#ffffff",
+        },
+        sidebar: {
+          // DEFAULT: 'hsl(var(--sidebar-background))',
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+          active: "hsl(var(--sidebar-active))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      animation: {
+        "shiny-text": "shiny-text 8s infinite",
+        "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+      },
+      keyframes: {
+        "shiny-text": {
+          "0%, 90%, 100%": {
+            "background-position": "calc(-100% - var(--shiny-width)) 0",
+          },
+          "30%, 60%": {
+            "background-position": "calc(100% + var(--shiny-width)) 0",
+          },
+        },
+        "border-beam": {
+          "100%": {
+            "offset-distance": "100%",
+          },
+        },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;

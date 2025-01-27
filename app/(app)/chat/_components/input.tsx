312d69fe95a495b3c9279cac0ea3c146e07dd7cc
vodinhvien <vodinhvien.work@gmail.com>
2025-01-27 16:32:01 +0700

    fix: chat empty

--- app/(app)/chat/_components/input.tsx
+++ app/(app)/chat/_components/input.tsx
@@ -1,68 +1,83 @@
 "use client";
 
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 
-import { CornerDownRight } from 'lucide-react';
+import Textarea from "react-textarea-autosize";
 
-import Textarea from 'react-textarea-autosize'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui";
 
-import { Button, Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from '@/components/ui';
+import { useEnterSubmit } from "../_hooks";
 
-import { useEnterSubmit } from '../_hooks';
+import { useChat } from "../_contexts/chat";
 
-import { useChat } from '../_contexts/chat';
+import { cn } from "@/lib/utils";
 
-import { cn } from '@/lib/utils';
-
-import ModelSelector from './model-selector';
-import { usePrivy } from '@privy-io/react-auth';
+import { usePrivy } from "@privy-io/react-auth";
+import Image from "next/image";
 
 const ChatInput: React.FC = () => {
+  const { user } = usePrivy();
 
-    const { user } = usePrivy();
+  const {
+    input,
+    setInput,
+    onSubmit,
+    isLoading,
+    model,
+    setModel,
+    inputDisabledMessage,
+  } = useChat();
 
-    const { input, setInput, onSubmit, isLoading, model, setModel, inputDisabledMessage } = useChat();
+  const { onKeyDown } = useEnterSubmit({ onSubmit: onSubmit });
 
-    const { onKeyDown } = useEnterSubmit({ onSubmit: onSubmit })
+  const inputRef = useRef<HTMLTextAreaElement>(null);
 
-    const inputRef = useRef<HTMLTextAreaElement>(null)
-
-    return (
-        <form
-            onSubmit={(e) => {
-                e.preventDefault();
-                onSubmit();
-            }}
-            className={cn(
-                // Base styles
-                "w-full rounded-md flex flex-col overflow-hidden transition-colors duration-200 ease-in-out border border-transparent shadow-none",
-                // Light mode styles
-                "bg-neutral-100 focus-within:border-white/5",
-                // Dark mode styles
-                "dark:bg-white/5 backdrop-blur-md/50 dark:focus-within:border-white/5",
-                isLoading && "opacity-50 cursor-not-allowed"
-            )}
-        >
-            <OptionalTooltip text={inputDisabledMessage}>
-                <Textarea
-                    ref={inputRef}
-                    tabIndex={0}
-                    onKeyDown={onKeyDown}
-                    placeholder="Ask Synnax AI anything..."
-                    className={cn(
-                        "w-full max-h-60 resize-none bg-transparent px-3 py-3 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-600 dark:placeholder:text-neutral-400 disabled:cursor-not-allowed disabled:opacity-50",
-                        "focus-visible:outline-none",
-                        "dark:placeholder:text-neutral-400",
-                    )}
-                    value={input}
-                    onChange={e => {
-                        setInput(e.target.value);
-                    }}
-                    disabled={isLoading || !user || inputDisabledMessage !== ''}
-                    autoFocus
-                />
-            </OptionalTooltip>
-            <div className="flex items-center justify-between px-2 pb-2">
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        onSubmit();
+      }}
+      className={cn(
+        // Base styles
+        "w-full relative rounded-xl border border-solid flex items-center justify-center overflow-hidden transition-colors duration-200 ease-in-out min-h-[52px] shadow-none",
+        // Light mode styles
+        "bg-neutral-100 focus-within:border-white/5",
+        // Dark mode styles
+        "dark:bg-white/5 backdrop-blur-md/50 dark:focus-within:border-white/5",
+        isLoading && "opacity-50 cursor-not-allowed"
+      )}
+    >
+      <OptionalTooltip text={inputDisabledMessage}>
+        <Textarea
+          ref={inputRef}
+          tabIndex={0}
+          onKeyDown={onKeyDown}
+          placeholder="Talk..."
+          className={cn(
+            "w-full max-h-60 resize-none bg-transparent px-3 py-3 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-600 dark:placeholder:text-neutral-400 disabled:cursor-not-allowed disabled:opacity-50",
+            "focus-visible:outline-none",
+            "dark:placeholder:text-neutral-400"
+          )}
+          value={input}
+          onChange={e => {
+            setInput(e.target.value);
+          }}
+          disabled={isLoading || !user || inputDisabledMessage !== ""}
+          autoFocus
+        />
+      </OptionalTooltip>
+      <div className={cn("pr-2 cursor-pointer h-fit")}>
+        <button type="submit" className="p-2">
+          <Image src="/icons/send.svg" alt="Logo" width={18} height={18} />
+        </button>
+      </div>
+      {/* <div className="flex items-center justify-between px-2 pb-2">
                 <ModelSelector
                     model={model}
                     onModelChange={setModel}
@@ -85,23 +100,28 @@ const ChatInput: React.FC = () => {
                         <TooltipContent>Send message</TooltipContent>
                     </Tooltip>
                 </TooltipProvider>
-            </div>
-        </form>
-    );
+            </div> */}
+    </form>
+  );
 };
 
-const OptionalTooltip = ({ children, text }: { children: React.ReactNode, text: string }) => {
+const OptionalTooltip = ({
+  children,
+  text,
+}: {
+  children: React.ReactNode;
+  text: string;
+}) => {
+  if (text === "") return children;
 
-    if(text === '') return children;
-
-    return (
-        <TooltipProvider>
-            <Tooltip delayDuration={0}>
-                <TooltipTrigger>{children}</TooltipTrigger>
-                <TooltipContent side="top">{text}</TooltipContent>
-            </Tooltip>
-        </TooltipProvider>
-    );
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent side="top">{text}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 };
 
 export default ChatInput;

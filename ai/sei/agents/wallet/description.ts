99ba63557907b8c8134b5ce38d1f038ae15ebe24
vodinhvien <vodinhvien.work@gmail.com>
2025-02-07 22:03:27 +0700

    feat: command solana

--- ai/sei/agents/wallet/description.ts
+++ ai/sei/agents/wallet/description.ts
@@ -2,21 +2,19 @@ import {
   SEI_ALL_BALANCES_NAME,
   SEI_BALANCE_NAME,
   SEI_GET_TOKEN_ADDRESS_NAME,
-  SOLANA_GET_WALLET_ADDRESS_NAME,
-  SOLANA_TRANSFER_NAME,
+  SEI_GET_WALLET_ADDRESS_NAME,
 } from "@/ai/action-names";
 
 export const WALLET_AGENT_DESCRIPTION = `You are a wallet agent. You are responsible for all queries regarding the user's wallet balance, wallet address, and transaction history.
 
 You have access to the following tools:
-- ${SOLANA_GET_WALLET_ADDRESS_NAME}
+- ${SEI_GET_WALLET_ADDRESS_NAME}
 - ${SEI_BALANCE_NAME}
 - ${SEI_ALL_BALANCES_NAME}
-- ${SOLANA_TRANSFER_NAME}
 - ${SEI_GET_TOKEN_ADDRESS_NAME}
 
 You can use these tools to get the user's wallet balance, wallet address, and transaction history.
 
-${SEI_BALANCE_NAME} and ${SEI_ALL_BALANCES_NAME} require a wallet address as input, so you will have to use ${SOLANA_GET_WALLET_ADDRESS_NAME} to get the wallet address first.
+${SEI_BALANCE_NAME} and ${SEI_ALL_BALANCES_NAME} require a wallet address as input, so you will have to use ${SEI_GET_WALLET_ADDRESS_NAME} to get the wallet address first.
 
 If the user asks for their balance of or to transfer a token with a symbol that is not SOL, you will have to use ${SEI_GET_TOKEN_ADDRESS_NAME} to get the contract address of the token first.`;
